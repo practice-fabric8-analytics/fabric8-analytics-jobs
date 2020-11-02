07d7d84b1690a62b0be5036d3390bed21369a337
Meer Sawood <msawood@redhat.com>
2020-11-02 18:44:38 +0530

    update scripts to use GH actions

--- qa/measure-maintainability-index.sh
+++ qa/measure-maintainability-index.sh
@@ -10,8 +10,12 @@ function prepare_venv() {
         # python36 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
-
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    if [ $? -eq 1 ]; then
+        # still don't have virtual environment -> use python3 directly
+        python3 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    else
+        ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    fi
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -19,7 +23,6 @@ function prepare_venv() {
 radon mi -s -i venv .
 
 popd
-
 defects="$(radon mi -s -n B -i venv . | wc -l)"
 if [[ $defects -gt 0 ]]
 then
